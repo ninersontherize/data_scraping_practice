b6fa6837734ca05e1da664ee3cc65ec7471f7490
Ryan McNicol <rmcnicol@alg.com>
2019-09-10 13:19:04 -0700

    got the scraping workinggit add -A

--- puppeteer_data_grab.js
+++ puppeteer_data_grab.js
@@ -7,9 +7,30 @@ function run() {
       const page = await browser.newPage();
       await page.goto("https://u.gg/lol/tier-list?role=support");
       await page.click('#qcCmpButtons > button:nth-child(2)');
+      await page.waitForSelector("strong.champion-name");
       let urls = await page.evaluate(() => {
-        let body_html = new XMLSerializer().serializeToString(document.doctype) + document.documentElement.outerHTML;
-        console.log(body_html);
+        let results = [];
+        let ranks = Array.prototype.slice.apply(document.querySelectorAll('div.rt-td.rank'));
+        let roles = Array.prototype.slice.apply(document.querySelectorAll('img.tier-list-role'));
+        let champions = Array.prototype.slice.apply(document.querySelectorAll('strong.champion-name'));
+        let tiers = Array.prototype.slice.apply(document.querySelectorAll('div.rt-td.tier'));
+        let winrates = Array.prototype.slice.apply(document.querySelectorAll('div.rt-td.winrate.highlight'));
+        let pickrates = Array.prototype.slice.apply(document.querySelectorAll('div.rt-td.pickrate'));
+        let banrates = Array.prototype.slice.apply(document.querySelectorAll('div.rt-td.banrate'));
+        let matches_played = Array.prototype.slice.apply(document.querySelectorAll('div.rt-td.matches'));
+        for(var i = 0; i < champions.length; i++) {
+          results.push({
+            rank: ranks[i].textContent,
+            role: roles[i].getAttribute('alt'),
+            champion: champions[i].textContent,
+            tier: tiers[i].textContent,
+            winrate: winrates[i].textContent,
+            pickrate: pickrates[i].textContent,
+            banrate: banrates[i].textContent,
+            matches_played: matches_played[i].textContent
+          });
+        };
+        return results;
       });
       browser.close();
       return resolve(urls);
@@ -17,7 +38,6 @@ function run() {
       return reject(e);
     }
   })
-
 }
 
 run().then(console.log).catch(console.error);
